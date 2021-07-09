018ee88488e25e48c9d790967f1826eb4c7b36f0
Dennis Simon <Dennis_Simon@external.mckinsey.com>
2021-07-09 12:25:05 +0200

    port to python3 (maybe)

--- src/pycall.c
+++ src/pycall.c
@@ -19,42 +19,46 @@ This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS O
 #include <usual/pgutil.h>
 
 char *pycall(PgSocket *client, char *username, char *query_str, char *py_file,
-		char* py_function) {
+			 char *py_function)
+{
 	PyObject *pName = NULL, *pModule = NULL, *pFunc = NULL;
 	PyObject *pArgs = NULL, *pValue = NULL;
 	PyObject *ptype, *perror, *ptraceback;
 	char *py_pathtmp, *py_filetmp, *py_path, *py_module, *ext;
 	char *res = NULL;
 
-        /* setup python search path */
+	/* setup python search path */
 	py_pathtmp = strdup(py_file);
-	if (py_pathtmp == NULL) {
+	if (py_pathtmp == NULL)
+	{
 		slog_error(client, "out of memory");
 		return NULL;
 	}
-	py_path = malloc(strlen(py_file) + 20) ;
-	if (py_path == NULL) {
+	py_path = malloc(strlen(py_file) + 20);
+	if (py_path == NULL)
+	{
 		slog_error(client, "out of memory");
 		free(py_pathtmp);
 		return NULL;
 	}
-        sprintf(py_path,"PYTHONPATH=%s",dirname(py_pathtmp)) ;
-	putenv(py_path) ;
+	sprintf(py_path, "PYTHONPATH=%s", dirname(py_pathtmp));
+	putenv(py_path);
 
 	/* setup python module name, function name */
 	py_filetmp = strdup(py_file);
-	if (py_filetmp == NULL) {
+	if (py_filetmp == NULL)
+	{
 		slog_error(client, "out of memory");
 		free(py_pathtmp);
 		free(py_path);
 		return NULL;
 	}
-	py_module = (char *) basename(py_filetmp);
+	py_module = (char *)basename(py_filetmp);
 	ext = strrchr(py_module, '.');
 	if (ext)
 		ext[0] = '\0';
 
-        /* Initialize the Python interpreter
+	/* Initialize the Python interpreter
          * NOTE: This call is a no-op on subsequent calls, as we do not 
          * call PyFinalize(). This 
          * a) avoids the overhead of repeatedly reloading the interpreter
@@ -64,65 +68,77 @@ char *pycall(PgSocket *client, char *username, char *query_str, char *py_file,
 	Py_Initialize();
 
 	/* Load python module */
-	pName = PyString_FromString(py_module);
-	if (pName == NULL) {
+	pName = PyUnicode_FromString(py_module);
+	if (pName == NULL)
+	{
 		slog_error(client, "Python module <%s> did not load", py_module);
 		goto finish;
 	}
 	pModule = PyImport_Import(pName);
-	if (pModule == NULL) {
+	if (pModule == NULL)
+	{
 		slog_error(client, "Python module <%s> did not load", py_module);
 		goto finish;
 	}
 
 	/* Prepare to call python function */
 	pFunc = PyObject_GetAttrString(pModule, py_function);
-	if (!pFunc) {
+	if (!pFunc)
+	{
 		slog_error(client, "Python Function <%s> not found in module <%s>",
-				py_function, py_module);
+				   py_function, py_module);
 		goto finish;
 	}
-	if (!PyCallable_Check(pFunc)) {
+	if (!PyCallable_Check(pFunc))
+	{
 		slog_error(client,
-				"Python Function <%s> in module <%s> is not callable!",
-				py_function, py_module);
+				   "Python Function <%s> in module <%s> is not callable!",
+				   py_function, py_module);
 		goto finish;
 	}
 
 	/* Call function with two arguments - username and query_str */
 	pArgs = PyTuple_New(2);
-	if (pArgs == NULL) {
+	if (pArgs == NULL)
+	{
 		slog_error(client, "Python module <%s>: out of memory", py_module);
 		goto finish;
 	}
-	pValue = PyString_FromString(username);
-	if (pValue == NULL) {
+	pValue = PyUnicode_FromString(username);
+	if (pValue == NULL)
+	{
 		slog_error(client, "Python module <%s>: out of memory", py_module);
 		goto finish;
 	}
 	PyTuple_SetItem(pArgs, 0, pValue);
-	pValue = PyString_FromString(query_str);
-	if (pValue == NULL) {
+	pValue = PyUnicode_FromString(query_str);
+	if (pValue == NULL)
+	{
 		slog_error(client, "Python module <%s>: out of memory", py_module);
 		goto finish;
 	}
 	PyTuple_SetItem(pArgs, 1, pValue);
 	pValue = PyObject_CallObject(pFunc, pArgs);
-	if (pValue == NULL) {
+	if (pValue == NULL)
+	{
 		slog_error(client, "Python Function <%s> failed to return a value",
-				py_function);
+				   py_function);
 		goto finish;
 	}
-	if (PyString_Check(pValue)) {
-		res = strdup(PyString_AsString(pValue));
-	} else {
+	if (PyUnicode_Check(pValue))
+	{
+		res = strdup(PyUnicode_AsString(pValue));
+	}
+	else
+	{
 		res = NULL;
 	}
 
-	finish:
-	if (PyErr_Occurred()) {
+finish:
+	if (PyErr_Occurred())
+	{
 		PyErr_Fetch(&ptype, &perror, &ptraceback);
-		slog_error(client, "Python error: %s", PyString_AsString(perror));
+		slog_error(client, "Python error: %s", PyUnicode_AsString(perror));
 	}
 	free(py_pathtmp);
 	free(py_filetmp);
@@ -134,4 +150,3 @@ char *pycall(PgSocket *client, char *username, char *query_str, char *py_file,
 	Py_XDECREF(pValue);
 	return res;
 }
-
